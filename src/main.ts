0edb07d01b8377026cd65047adf73c834d214669
brolly <102495553@qq.com>
2021-05-19 13:29:42 +0800

    feat(src/main.ts): css初始化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 初始化css
+import 'normalize.css/normalize.css'
+// element-plus
 import installElementPlus from './plugins/element'
 
 const app = createApp(App)
--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -10,7 +10,7 @@ import lang from 'element-plus/lib/locale/lang/zh-cn'
 // Element Plus 直接使用了 Day.js 项目的时间日期国际化设置, 并且会自动全局设置已经导入的 Day.js 国际化配置。
 import 'dayjs/locale/zh-cn'
 
-export default (app: App) => {
+export default (app: App): void => {
   locale(lang)
 
   // 按需导入组件列表
